61fd4a220ef80fbdb7ecee1b1d91ff4657a81b53
juanPedroBasualdo <jpbasualdo@fi.uba.ar>
2025-11-26 20:24:30 -0300

    Hecho refactor a Puerto y Tablero para que no rompa encapsulamiento

---
+++ src/main/java/edu/fiuba/algo3/modelo/excepciones/NoTieneRecursosParaIntercambioException.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.excepciones;
+
+public class NoTieneRecursosParaIntercambioException extends PuertoException{
+    public NoTieneRecursosParaIntercambioException(String s) {
+        super();
+    }
+}
--- src/main/java/edu/fiuba/algo3/modelo/tablero/puerto/PuertoEspecifico2_1.java
+++ src/main/java/edu/fiuba/algo3/modelo/tablero/puerto/PuertoEspecifico2_1.java
@@ -1,65 +1,53 @@
 package edu.fiuba.algo3.modelo.tablero.puerto;
 
+import edu.fiuba.algo3.modelo.excepciones.NoTieneRecursosParaIntercambioException;
+import edu.fiuba.algo3.modelo.excepciones.PuertoNoAdyacenteException;
 import edu.fiuba.algo3.modelo.jugador.Jugador;
 import edu.fiuba.algo3.modelo.tablero.Recurso;
-import edu.fiuba.algo3.modelo.tablero.coordenada.Coordenada;
-import edu.fiuba.algo3.modelo.tablero.terreno.parte.Arista;
+import edu.fiuba.algo3.modelo.tablero.terreno.parte.Vertice;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class PuertoEspecifico2_1 implements Puerto {
 
-    private final HashMap<Recurso, List<Coordenada>> listasDeVertices;
+    private final HashMap<Recurso, List<Vertice>> verticesDePuerto;
 
     // TODO se supone que el puerto tiene que estar en una arista para que Jugador la pueda usar, discutir en llamada
 
-    public PuertoEspecifico2_1() {
-        this.listasDeVertices = new HashMap<Recurso, List<Coordenada>>();
-
-        listasDeVertices.put(Recurso.CEREAL, Arrays.asList(
-                new Coordenada(0, 1, 0),
-                new Coordenada(0, 1, 1)
-        ));
-
-        listasDeVertices.put(Recurso.MADERA, Arrays.asList(
-                new Coordenada(1, 0, 5),
-                new Coordenada(1, 0, 4)
-        ));
-
-        listasDeVertices.put(Recurso.MINERAL, Arrays.asList(
-                new Coordenada(1, 3, 0),
-                new Coordenada(1, 3, 1)
-        ));
-
-        listasDeVertices.put(Recurso.ARCILLA, Arrays.asList(
-                new Coordenada(3, 0, 5),
-                new Coordenada(3, 0, 4)
-        ));
-
-        listasDeVertices.put(Recurso.LANA, Arrays.asList(
-                new Coordenada(3, 3, 2),
-                new Coordenada(3, 3, 3)
-        ));
+    public PuertoEspecifico2_1(HashMap<Recurso, List<Vertice>> verticesDePuerto) {
+        this.verticesDePuerto = verticesDePuerto;
     }
 
     @Override
     public void intercambiar(Jugador jugador, Recurso recursoACambiar, Recurso recursoARecibir) {
+        if(puedeIntercambiar(jugador, recursoACambiar)) {
+            List<Recurso> listaRequiriente = Arrays.asList(recursoACambiar, recursoACambiar);
 
-        List<Recurso> listaRequiriente = Arrays.asList(recursoACambiar, recursoACambiar);
+            if (!jugador.tieneRecursos(listaRequiriente)) {
+                throw new NoTieneRecursosParaIntercambioException("El jugador no tiene los recursos necesarios para intercambiar 2:1");
+            }
 
-        if (!jugador.tieneRecursos(listaRequiriente)) {
-            throw new IllegalStateException("El jugador no tiene los recursos necesarios para intercambiar 2:1");
+            jugador.eliminarRecursos(listaRequiriente);
+            jugador.agregarRecurso(recursoARecibir);
+        } else {
+            throw new PuertoNoAdyacenteException("El jugador no se encuentra adyacente al puerto");
         }
-
-        jugador.eliminarRecursos(listaRequiriente);
-        jugador.agregarRecurso(recursoARecibir);
     }
 
-    public List<Coordenada> obtenerCoordenadaPuertoDe(Recurso recurso) {
-        return listasDeVertices.get(recurso);
+    public boolean puedeIntercambiar(Jugador jugador, Recurso recurso) {
+        List<Vertice> verticesDelIntercambio = this.obtenerVerticesPuertoDe(recurso);
+        for(Vertice v : verticesDelIntercambio) {
+            if(v.tienePropietario(jugador)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Vertice> obtenerVerticesPuertoDe(Recurso recurso) {
+        return verticesDePuerto.get(recurso);
     }
 
 }
